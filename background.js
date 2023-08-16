3f8e47320b697af1abbc2d72fd9291649d19f85b
Noah Humphreys <granoahbar@gmail.com>
2023-08-15 20:52:55 -0600

    no good

--- background.js
+++ background.js
@@ -1,14 +1,17 @@
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//   if (request.action === "openTabs") {
+//     const urls = request.urls;
+//     urls.forEach((url) => {
+//       chrome.tabs.create({ url: url });
+//     });
+//     sendResponse({ message: "Tabs opened" });
+//   }
+// });
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "openTabs") {
-      const urls = request.urls;
-      urls.forEach((url) => {
-        chrome.tabs.create({ url: url });
-      });
-      sendResponse({ message: "Tabs opened" });
-    }
-  })
-  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "insertCrosshairs") {
-      sendResponse({ message: "Crosshairs Inserted" });
-    }
-  })
+  if (request.action === "insertIcons") {
+    sendResponse({ message: "Icons Inserted" });
+  } else if (request.action === "removeIcons") {
+    sendResponse({ message: "Icons Removed" });
+  }
+});
--- content.js
+++ content.js
@@ -1,6 +1,13 @@
-chrome.runtime.sendMessage({ action: "openTabs", urls: matchingUrls }, (response) => {
-    console.log("Message sent to background script");
-  });
-  chrome.runtime.sendMessage({ action: "insertCrosshairs", urls: matchingUrls }, (response) => {
-    console.log("Message sent to background script");
-  });
+// chrome.runtime.sendMessage({ action: "openTabs", urls: matchingUrls }, (response) => {
+//   console.log("Message sent to background script");
+// });
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "insertIcons") {
+    // Call the function to insert icons from popup.js
+    insertIcons();
+  } else if (message.action === "removeIcons") {
+    // Call the function to remove icons from popup.js
+    removeIcons();
+  }
+});
