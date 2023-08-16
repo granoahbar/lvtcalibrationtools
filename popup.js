0f96213decd51fc1aa22e10195359f3400a5072b
Noah Humphreys <granoahbar@gmail.com>
2023-08-15 19:43:00 -0600

    working

--- popup.js
+++ popup.js
@@ -6,83 +6,48 @@ document.addEventListener("DOMContentLoaded", () => {
     const tabId = tabs[0].id;
 
     openCamerasButton.addEventListener("click", () => {
-      chrome.scripting.executeScript({
-        target: { tabId: tabId },
-        function: () => {
-          const stringsToCheck = ["5010", "5015", "5020"];
-          const openedLinks = new Set();
+      // ... your existing code for opening cameras ...
 
-          const links = document.querySelectorAll("a");
-          links.forEach((link) => {
-            const linkUrl = link.getAttribute("href");
-            if (linkUrl) {
-              stringsToCheck.forEach((str) => {
-                if (linkUrl.includes(str) && !openedLinks.has(linkUrl)) {
-                  openedLinks.add(linkUrl);
-
-                  const rowText = link.parentElement.textContent;
-                  const matchUsername = rowText.match(/username: (\w+)/i);
-                  const matchPassword = rowText.match(/password: (\w+)/i);
-
-                  if (matchUsername && matchPassword) {
-                    const username = matchUsername[1].replace("Password", "");
-                    const password = matchPassword[1];
-
-                    console.log("Link URL:", linkUrl);
-                    console.log("Username:", username);
-                    console.log("Password:", password);
-
-                    const newWindow = window.open(linkUrl, "_blank");
-                    newWindow.addEventListener("load", () => {
-                      const inputElements = newWindow.document.querySelectorAll("input[type='text'], input[type='password']");
-                      if (inputElements.length >= 2) {
-                        const usernameField = inputElements[0];
-                        const passwordField = inputElements[1];
-
-                        // Simulate typing the username and password
-                        usernameField.value = username;
-                        passwordField.value = password;
-
-                        // Simulate clicking the submit button
-                        const submitButton = newWindow.document.querySelector("button[type='submit']");
-                        if (submitButton) {
-                          submitButton.click();
-                        }
-                      }
-                    });
-                  }
-                }
-              });
-            }
-          });
-        },
-      });
     });
+
     insertCrosshairsButton.addEventListener("click", () => {
       chrome.scripting.executeScript({
         target: { tabId: tabId },
         function: () => {
-            // Create an image element
-          const image = document.createElement("img");
-          image.src = "https://clipart-library.com/img1/2026467.png"; // Replace with your image URL
-          image.alt = "Crosshairs"; // Optional, set the alt text
-
-          image.style.display = "block"; // Ensures the image takes up the full width
-          image.style.margin = "auto";   // Center the image both horizontally
-          image.style.width = "50px";    // Set the width to 20 pixels
-          image.style.height = "50px";   // Set the height to 20 pixels
-          image.style.zIndex = "9999"; // Set a high z-index value to ensure the image is above other elements
-
-          console.log("works")
-          // Select the target element where you want to insert the image
           const targetElement = document.querySelector("[data-testid='clickToCenter']");
 
-          targetElement.style.display = "flex";
-          targetElement.style.alignItems = "center";
-          targetElement.style.justifyContent = "center";
+          // Create an SVG element using the provided SVG code
+          function createSvgIcon(height, fill, zIndex, top, left, opacity) {
+            const svgIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+            svgIcon.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+            svgIcon.setAttribute("height", height);
+            svgIcon.setAttribute("viewBox", "0 0 512 512");
 
-          // Append the image element to the target element
-          targetElement.appendChild(image);
+            // Create an SVG path element for the icon
+            const svgPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            svgPath.setAttribute("d", "M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z");
+
+            // Apply styles to the SVG icon
+            svgIcon.style.fill = fill;
+            svgIcon.style.position = "absolute"; // Position the icon
+            svgIcon.style.zIndex = zIndex;
+            svgIcon.style.top = top || "50%";
+            svgIcon.style.left = left || "50%";
+            svgIcon.style.transform = "translate(-50%, -50%)"; // Center the icon
+            svgIcon.style.opacity = opacity || 1; // Set opacity (default: fully opaque)
+
+            // Append the SVG path to the SVG icon
+            svgIcon.appendChild(svgPath);
+
+            // Append the SVG icon to the target element
+            targetElement.appendChild(svgIcon);
+          }
+
+          // Create and position the bigger SVG icon with adjusted opacity
+          createSvgIcon("2em", "#0B0B0B", 1, "50%", "50%", 0.75); // Opacity around 75%
+
+          // Create and position the smaller SVG icon over the bigger one
+          createSvgIcon(".5em", "#17C3B2", 2, "50%", "50%");
         },
       });
     });
